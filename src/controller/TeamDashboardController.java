2efceaeceb40e22fb5780de5ae363e35aa6ab893
rmimpact <rmimpactfn@gmail.com>
2025-10-13 23:08:59 +1100

    Active Team is set up and binded to the UI

--- src/controller/TeamDashboardController.java
+++ src/controller/TeamDashboardController.java
@@ -1,11 +1,11 @@
 
 /*--------------- To Do -------------------
-        - handleUnsign
-        - Refresh table Function
-        - Finalise Table + Active team views
+        - handleUnsign -> pending
+        - Refresh table Function -> done
+        - Finalise Table + Active team views -> incomplete
         - When an image on the active team is clicked
         unsign that player from the active team but
-        keep them in the main team.
+        keep them in the main team. -> incomplete
 */
 
 
@@ -18,6 +18,7 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 import model.application.*;
 import javafx.scene.control.Button;
@@ -35,95 +36,98 @@ public class TeamDashboardController extends Controller<League> {
     @FXML Button unsignPlayerButton;
 
 
-    @FXML
-    private void initialize() {
-        System.out.println("Initializing TeamDashboardController");
-        System.out.println("Managing: " + model.getLoggedInManager().getTeam());
-                //getLoggedInManager().getTeam());
-        teamLabel.setText(model.getLoggedInManager().getTeam().toString());
-        refresh();
-
-            // Wait until both controls are ready
-            playerTable.sceneProperty().addListener((obs, oldScene, newScene) -> {
-                if (newScene != null && unsignPlayerButton != null) {
-                    unsignPlayerButton.disableProperty().bind(
-                            playerTable.getSelectionModel().selectedItemProperty().isNull()
-                    );
-                }
-            });
-
-        System.out.println("playerTable = " + playerTable);
-        System.out.println("playerNameColumn = " + playerNameColumn);
-        System.out.println("playerPositionColumn = " + playerPositionColumn);
-        System.out.println("Team players = " + model.getLoggedInManager().getTeam().getAllPlayers().getPlayers());
-
-        for (Player player : model.getLoggedInManager().getTeam().getAllPlayers().getPlayers()) {
-            System.out.println(player);
-        }
-        System.out.println("-----------------^^^ Team ^^^----------------------");
+    //---- Images -----
+    @FXML ImageView topMidPlayerImage;
+    @FXML ImageView midLeftPlayerImage;
+    @FXML ImageView midMidPlayerImage;
+    @FXML ImageView midRightPlayerImage;
+    @FXML ImageView botMidPlayerImage;
 
 
-        //playerNameColumn.setCellValueFactory(cellData -> cellData.getValue().fullNameProperty());
-        //playerPositionColumn.setCellValueFactory(cellData -> cellData.getValue().positionProperty());
-        //playerTable.setItems(model.getPlayers().getPlayers());
-
-
-
-        int playerCount = 0;
-        for (Player player : model.getPlayers().getPlayers()) {
-            /*Debugging*/ System.out.println(player);
-            playerCount++;
-        }
-        /*Debugging*/ System.out.println("Players count: " + playerCount);
-
+    @FXML public void handleTopMidPlayerClick() {
+        /*Debugging*/ System.out.println("Clicked Top Mid Player");
     }
 
+    @FXML public void handleMidLeftPlayerClick() {
+        /*Debugging*/ System.out.println("Clicked Mid Left Player");
+    }
+
+    @FXML public void handleMidMidPlayerImageClick() {
+        /*Debugging*/ System.out.println("Clicked Mid Mid Player");
+    }
+
+    @FXML public void handleMidRightPlayerImageClick() {
+        /*Debugging*/ System.out.println("Clicked Mid Right Player");
+    }
+
+    @FXML public void handleBotMidPlayerImageClick() {
+        /*Debugging*/ System.out.println("Clicked Bot Mid Player");
+    }
+
+
+
+
+
+
+
+    @FXML
+    private void initialize() {
+        System.out.println("mode.getLoggedInManager().getTeam() = " + model.getLoggedInManager().getTeam().getAllPlayers().getPlayers());
+        playerNameColumn.setCellValueFactory(cellData -> cellData.getValue().fullNameProperty());
+        playerPositionColumn.setCellValueFactory(cellData -> cellData.getValue().positionProperty());
+        playerTable.setItems(model.getLoggedInManager().getTeam().getAllPlayers().getPlayers());
+        playerTable.getSelectionModel().selectedItemProperty().addListener((obs, old, nw) -> {
+            unsignPlayerButton.setDisable(nw == null);
+        });
+        playerTable.refresh();
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+    //----- Signing --------------------------------------------------------------
     @FXML
     private void handlePlayerSign() {
         /*Debugging*/ System.out.println("Sign button clicked.");
         try{
             String search = playerSearchTextField.getText();
             Player player = model.getPlayers().player(search);
+            Team team = player.getTeam();
+            if (team == model.getLoggedInManager().getTeam()) {
+                showError(player.getFullName() + " is already signed to your team", "InvalidSigningException");
+            }
+            else if (team != null) {
+                showError("Cannot sign " + player.getFullName() + ", player is already signed to " + player.getTeam(), "InvalidSigningException");
+            }
+            else {
+                model.getPlayers().player(search).setTeam(model.getLoggedInManager().getTeam());
+                model.getLoggedInManager().getTeam().getAllPlayers().add(player);
+                /*Debugging*/ System.out.println("Signing "+ model.getPlayers().player(search) + " to " + model.getLoggedInManager().getTeam());
+            }
 
-            model.getPlayers().player(search).setTeam(model.getLoggedInManager().getTeam());
-            model.getLoggedInManager().getTeam().getAllPlayers().add(player);
-
-            refresh();
-            /*Debugging*/ System.out.println("Signing "+ model.getPlayers().player(search) + " to " + model.getLoggedInManager().getTeam());
         }
         catch (NullPointerException e){
             showError("Player does not exist within the league", "InvalidSigningException");
         }
     }
 
-    private void showError(String description, String name) {
-        try {
-            ErrorController.setErrorMessage(description, name);
-            ViewLoader.showStage(null, "/view/ErrorView.fxml", "Error", new Stage());
-        }
-        catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    @FXML
-    private void handleClose() {
-        this.stage.close() ;
-    }
-
     @FXML
     private void handlePlayerUnsign() {
-
         System.out.println("Unsign button clicked.");
         Player player = playerTable.getSelectionModel().getSelectedItem();
         player.setTeam(null);
         model.getLoggedInManager().getTeam().getAllPlayers().remove(player);
-        System.out.println("Unsigning "+ player);
-
-        refresh();
-        //model.getLoggedInManager().getTeam().getAllPlayers().;
-        //model.getLoggedInManager().getTeam().getAllPlayers().remove(player);
+        /*Debugging*/ System.out.println("Unsigning " + player);
     }
+    //^^^^^ Signing ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 
     @FXML
     private void handleKeyRelease() {
@@ -137,15 +141,18 @@ public class TeamDashboardController extends Controller<League> {
         //Exception Handled
     }
 
-    private void refresh() {
-        //playerTable.getItems().clear();
-
-        System.out.println("mode.getLoggedInManager().getTeam() = " + model.getLoggedInManager().getTeam().getAllPlayers().getPlayers());
-        playerNameColumn.setCellValueFactory(cellData -> cellData.getValue().fullNameProperty());
-        playerPositionColumn.setCellValueFactory(cellData -> cellData.getValue().positionProperty());
-        playerTable.setItems(model.getLoggedInManager().getTeam().getAllPlayers().getPlayers());
-        playerTable.refresh();
+    @FXML
+    private void handleClose() {
+        this.stage.close() ;
     }
 
-
+    private void showError(String description, String name) {
+        try {
+            ErrorController.setErrorMessage(description, name);
+            ViewLoader.showStage(null, "/view/ErrorView.fxml", "Error", new Stage());
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
